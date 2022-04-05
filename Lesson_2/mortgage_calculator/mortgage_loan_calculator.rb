4e4e01d54e98446665c12a213bec4c4b9bf2b1c0
Ashley <schuurmanashley@gmail.com>
2022-04-05 09:54:45 -0700

    add functionality to mortgage loan calculator; sorted into folders

--- Lesson_2/mortgage_calculator/mortgage_loan_calculator.rb
+++ Lesson_2/mortgage_calculator/mortgage_loan_calculator.rb
@@ -1,16 +1,44 @@
 require 'yaml'
 MESSAGES = YAML.load_file('mortgage_loan_messages.yml')
 
+# methods
 def prompt(message)
   puts "=> #{message}"
 end
 
-def valid_number?(num)
-  Float(num) != nil rescue false
+def integer?(num)
+  num.to_i.to_s == num
 end
 
-def number_to_money(number)
-  number.round(2)
+def float?(num)
+  num.to_f.to_s == num
+end
+
+def valid_number?(num)
+  (num.to_i > 0) && (integer?(num) || float?(num))
+end
+
+def remove_spec_char(str)
+  str.gsub!(/[$,]/, '')
+end
+
+def money_format(num)
+  "$#{num.round(2)}"
+end
+
+def get_input(msg, )
+  loop do
+    prompt(MESSAGES[msg])
+    input = gets.chomp
+
+    remove_spec_char(input)
+
+    if valid_number?(input)
+      break
+    else
+      prompt(MESSAGES['invalid_number'])
+    end
+  end
 end
 
 # Welcome
@@ -25,10 +53,13 @@ loop do
     prompt(MESSAGES['loan_amt_input'])
     loan_amount = gets.chomp
 
-    if valid_number?(loan_amount) == false
-      prompt(MESSAGES['invalid_number'])
-    else
+    # removes $ and , from loan input
+    money_unformat(loan_amount)
+
+    if valid_number?(loan_amount)
       break
+    else
+      prompt(MESSAGES['invalid_number'])
     end
   end
 
@@ -37,10 +68,10 @@ loop do
   loop do
     prompt(MESSAGES['apr_input'])
     apr = gets.chomp
-    if valid_number?(apr) == false
-      prompt(MESSAGES['invalid_number'])
-    else
+    if valid_number?(apr)
       break
+    else
+      prompt(MESSAGES['invalid_number'])
     end
   end
 
@@ -49,10 +80,10 @@ loop do
   loop do
     prompt(MESSAGES['loan_dur_input'])
     loan_duration = gets.chomp
-    if valid_number?(loan_duration) == false
-      prompt(MESSAGES['invalid_number'])
-    else
+    if valid_number?(loan_duration)
       break
+    else
+      prompt(MESSAGES['invalid_number'])
     end
   end
 
@@ -60,12 +91,12 @@ loop do
   annual_interest = apr.to_f / 100
   monthly_interest = annual_interest / 12
 
-  monthly_payment = loan_amount.to_f * 
+  monthly_payment = loan_amount.to_f *
                     (monthly_interest / 
                     (1 - (1 + monthly_interest)**(-loan_duration.to_f)))
-  
+
   # display result
-  prompt(MESSAGES['monthly_payment_result'] + "$#{number_to_money(monthly_payment)}.")
+  prompt(MESSAGES['monthly_payment_result'] + money_format(monthly_payment))
 
   # loop again?
   prompt(MESSAGES['calculate_again'])

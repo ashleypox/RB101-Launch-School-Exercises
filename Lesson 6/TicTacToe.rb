e71717a7b1cf2d91154850340afb48f54af93d02
Ashley <schuurmanashley@gmail.com>
2022-04-27 10:53:02 -0700

    Lesson 6 Tic Tac Toe part 1

---
+++ Lesson 6/TicTacToe.rb
@@ -0,0 +1,22 @@
+# start with a description of the game!
+
+# Tic Tac Toe is a 2 player game on a 3x3 board
+# each player takes a turn and marks a square on the board
+# the first platyer to reach 3 sqaures in a row, including diagonals, wins
+# if all 9 sqaures get filled with no 3 in a row, the game is a tie
+
+
+# then, build out the sequence:
+
+# Display the empty 3 x 3 board
+# ask the user to pick a square
+# computer picks a square
+# display updated board state
+# loop this until either a win or a tie;
+  #if a winner, display winner
+  # if a tie, display tie
+# play again?
+# if yes, clear board and go to #1
+# good bye!
+
+
